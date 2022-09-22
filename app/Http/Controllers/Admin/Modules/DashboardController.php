93d80636895f85213691df4ce77a45990e46919e
PixelTijgers <57810027+PixelTijgers@users.noreply.github.com>
2022-09-22 08:42:09 +0200

    Changed DashboardController.

--- app/Http/Controllers/Admin/Modules/DashboardController.php
+++ app/Http/Controllers/Admin/Modules/DashboardController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Admin\Modules;
 use App\Http\Controllers\Controller;
 
 // Facades.
-use Analytics;
-use Spatie\Analytics\Period;
 
 // Models.
 use App\Models\Post;
@@ -29,15 +27,7 @@ class DashboardController extends Controller
     */
     public function index()
     {
-        // Fetch the visitors per page per period.
-        $visitorsDay = Analytics::fetchVisitorsAndPageViews(Period::days(1));
-        $visitorsWeek = Analytics::fetchVisitorsAndPageViews(Period::days(7));
-        $visitorsMonth = Analytics::fetchVisitorsAndPageViews(Period::days(30));
-
-        // Fetcht the top 5 browsers.
-        $browser = Analytics::fetchTopBrowsers(Period::days(30), 5);
-
         // Init view.
-        return view('admin.modules.dashboard.index', compact('visitorsDay', 'visitorsWeek', 'visitorsMonth', 'browser'));
+        return view('admin.modules.dashboard.index');
     }
 }
